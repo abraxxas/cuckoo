963e6c52928c9a0c8b00f0c4e03925d12dcfa3d2
Sebastian Dumbs <thesofty@gmail.com>
2016-05-04 17:55:46 +0200

    implemented member

--- ContDynArray.h
+++ ContDynArray.h
@@ -1,14 +1,14 @@
  #ifndef CONTDYNARRAY_H
  #define CONTDYNARRAY_H
 
- // ContDynArray.h
- //
- // UE Algorithmen und Datenstrukturen - SS 2016 Universitaet Wien
- // https://cewebs.cs.univie.ac.at/ADS/ss16/
- //
- // Beispielimplementierung mit Datenstruktur "unsortiertes Feld"
- // Achtung: Da hier keine effiziente Suchdatenstruktur zum Einsatz kommt,
- // ist das Laufzeitverhalten extrem schlecht.
+// ContDynArray.h
+//
+// UE Algorithmen und Datenstrukturen - SS 2016 Universitaet Wien
+// https://cewebs.cs.univie.ac.at/ADS/ss16/
+//
+// Beispielimplementierung mit Datenstruktur "unsortiertes Feld"
+// Achtung: Da hier keine effiziente Suchdatenstruktur zum Einsatz kommt,
+// ist das Laufzeitverhalten extrem schlecht.
 
  #include <iostream>
  #include "Container.h"
@@ -19,177 +19,194 @@
 
 
 
- class ContDynArrayEmptyException : public ContainerException {
- public:
-   virtual const char * what() const noexcept override { return "ContDynArray: empty"; }
- };
+class ContDynArrayEmptyException: public ContainerException {
+public:
+        virtual const char * what() const noexcept override {
+                return "ContDynArray: empty";
+        }
+};
 
- template <typename E, size_t N=7>
- class ContDynArray : public Container<E> {
-   size_t nmax;
-   size_t n;
-   E * values;
+template <typename E, size_t N=7>
+                                class ContDynArray: public Container<E> {
+        size_t nmax;
+        size_t n;
+        E * values;
 
-   //http://codereview.stackexchange.com/questions/35220/perfect-hashing-implementation
-   //http://liuluheng.github.io/wiki/public_html/Algorithms/Theory%20of%20Algorithms/Hash%20table.html
-   //http://users.cis.fiu.edu/~weiss/dsaa_c++4/code/
-   const int tMax = 10000; // max number of random walk trials
-   int K; // number of probes
-   size_t number_max; // number of max elements
-   size_t number; // number of current elements
-   size_t q=3;//exponent der aktuellen tabellengröße als 2er potenz
-   E * H1,H2;
-   size_t * H1_indices,H2_indices;
+        //http://codereview.stackexchange.com/questions/35220/perfect-hashing-implementation
+        //http://liuluheng.github.io/wiki/public_html/Algorithms/Theory%20of%20Algorithms/Hash%20table.html
+        //http://users.cis.fiu.edu/~weiss/dsaa_c++4/code/
+        const int tMax = 10000; // max number of random walk trials
+        int K; // number of probes
+        size_t number_max; // number of max elements
+        size_t number; // number of current elements
+        size_t q=3;//exponent der aktuellen tabellengröße als 2er potenz
+        E * H1;
+        E * H2;
+        size_t * H1_indices,H2_indices;
 
-   std::random_device rd;     // only used once to initialise (seed) engine
-
-
-   size_t a1 = random_nmbr(); //random numbers should be huge about 18-20 digits
-   size_t a2 = random_nmbr();
-
-   size_t random_nmbr(){
-     std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-     std::uniform_int_distribution<size_t> uni(0,SIZE_MAX); // guaranteed unbiased
-
-     return uni(rng);
-   }
-
-   // function for calculation of hash
-   size_t hash1(const E& e) const {
-     std::cout << "a1: " << a1 << "\n";
-     std::cout << "a2: " << a2 << "\n";
-     std::cout << "hashValue(e): " <<hashValue(e) << "\n";
-     std::cout << "a1*hashValue(e): " << a1*hashValue(e) << "\n";
-     std::cout << "CHAR_BIT*sizeof(size_t)-q: " << CHAR_BIT*sizeof(size_t)-q << "\n";
-     std::cout << "((a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q)): " << ((a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q))  << "\n";
-
-     return (a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q);
-   }
-   size_t hash2(const E& e) const {
-     return (a2*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q);
-   }
+        std::random_device rd; // only used once to initialise (seed) engine
 
 
-   void sort() const;
- public:
-   ContDynArray() : nmax{N}, n{0}, values{new E[this->nmax]} { }
-   ContDynArray(std::initializer_list<E> el) : ContDynArray() { for (auto e: el) add(e); }
+        size_t a1 = random_nmbr(); //random numbers should be huge about 18-20 digits
+        size_t a2 = random_nmbr();
 
-   virtual ~ContDynArray() { delete[] values; }
+        size_t random_nmbr(){
+                std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
+                std::uniform_int_distribution<size_t> uni(0,SIZE_MAX); // guaranteed unbiased
 
-   using Container<E>::add;
-   virtual void add(const E e[], size_t len) override;
+                return uni(rng);
+        }
 
-   using Container<E>::remove;
-   virtual void remove(const E e[], size_t len) override;
+        // function for calculation of hash
+        size_t hash1(const E& e) const {
+                /*std::cout << "a1: " << a1 << "\n";
+                   std::cout << "a2: " << a2 << "\n";
+                   std::cout << "hashValue(e): " <<hashValue(e) << "\n";
+                   std::cout << "a1*hashValue(e): " << a1*hashValue(e) << "\n";
+                   std::cout << "CHAR_BIT*sizeof(size_t)-q: " << CHAR_BIT*sizeof(size_t)-q << "\n";
+                   std::cout << "((a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q)): " << ((a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q))  << "\n";*/
 
-   virtual bool member(const E& e) const override;
-   virtual size_t size() const override { return n; }
+                return (a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q);
+        }
+        size_t hash2(const E& e) const {
+                return (a2*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q);
+        }
 
-   virtual E min() const override;
-   virtual E max() const override;
 
-   virtual std::ostream& print(std::ostream& o) const override;
+        void sort() const;
+public:
+        ContDynArray() : nmax {N}, n {0}, values {new E[this->nmax]}, H1 {new E[this->nmax]}, H2 {new E[this->nmax]} { }
+        ContDynArray(std::initializer_list<E> el) : ContDynArray() {
+                for (auto e: el) add(e);
+        }
 
-   virtual size_t apply(std::function<void(const E&)> f, Order order = dontcare) const override;
- };
+        virtual ~ContDynArray() {
+                delete[] values;
+        }
 
- template <typename E, size_t N>
- void ContDynArray<E,N>::add(const E e[], size_t len) {
-   for (size_t i = 0; i < len; ++i)
-       hash1(e[i]);
-   /*if (n + len > nmax) {
-     auto newnmax = nmax;
-     E * newvalues = nullptr;
-     while (n + len > newnmax) newnmax = (newnmax*12)/10 + 2;
-     newvalues = new E[newnmax];
-     for (size_t i = 0; i < n; ++i)
-       newvalues[i] = values[i];
-     delete[] values;
-     values = newvalues;
-     nmax = newnmax;
-   }
-   for (size_t i = 0; i < len; ++i)
-     if (!member(e[i]))
-       values[n++] = e[i];*/
- }
+        using Container<E>::add;
+        virtual void add(const E e[], size_t len) override;
 
- template <typename E, size_t N>
- void ContDynArray<E,N>::remove(const E e[], size_t len) {
-   for (size_t i = 0; i < len; ++i) {
-     for (size_t j = 0; j < n; ++j) {
-       if (values[j] == e[i]) {
-         values[j] = values[--n];
-         break;
-       }
-     }
-   }
- }
+        using Container<E>::remove;
+        virtual void remove(const E e[], size_t len) override;
 
- template <typename E, size_t N>
- bool ContDynArray<E,N>::member(const E& e) const {
-   for (size_t i = 0; i < n; ++i) {
-     if (values[i] == e) return true;
-   }
-   return false;
- }
+        virtual bool member(const E& e) const override;
+        virtual size_t size() const override {
+                return n;
+        }
 
- template <typename E, size_t N>
- E ContDynArray<E,N>::min() const {
-   if (this->empty()) throw ContDynArrayEmptyException();
-   E rc = values[0];
-   for (size_t i = 1; i < n; ++i) {
-     if (rc > values[i]) rc = values[i];
-   }
-   return rc;
- }
+        virtual E min() const override;
+        virtual E max() const override;
 
- template <typename E, size_t N>
- E ContDynArray<E,N>::max() const {
-   if (this->empty()) throw ContDynArrayEmptyException();
-   E rc = values[0];
-   for (size_t i = 1; i < n; ++i) {
-     if (values[i] > rc) rc = values[i];
-   }
-   return rc;
- }
+        virtual std::ostream& print(std::ostream& o) const override;
 
- template <typename E, size_t N>
- std::ostream& ContDynArray<E,N>::print(std::ostream& o) const {
-   o << "ContDynArray [ n=" << n << " nmax=" << nmax << " values=";
-   for (size_t i = 0; i < n; ++i) o << ' ' << values[i];
-   o << " ]";
-   return o;
- }
+        virtual size_t apply(std::function<void(const E &)> f, Order order = dontcare) const override;
+};
 
- template <typename E, size_t N>
- size_t ContDynArray<E,N>::apply(std::function<void(const E&)> f, Order order) const {
-   size_t rc = 0;
-   if (order != dontcare) sort();
-   try {
-     if (order == descending) {
-       for (size_t i = n; i--;) {
-         f(values[i]);
-         ++rc;
-       }
-     } else {
-       for (size_t i = 0; i < n; ++i) {
-         f(values[i]);
-         ++rc;
-       }
-     }
-   } catch (...) {}
-   return rc;
- }
+template <typename E, size_t N>
+void ContDynArray<E,N>::add(const E e[], size_t len) {
+        for (size_t i = 0; i < len; ++i) {
+                H1[hash1(e[i])] = e[i];
+                std::cout << "H1[hash1(e[i])]: " << H1[hash1(e[i])] << "\n";
+        }
 
- template <typename E, size_t N>
- void ContDynArray<E,N>::sort() const { // Achtung, O(n*n)
-   for (size_t i = 0; i < n; ++i) {
-     size_t min = i;
-     for (size_t j = i+1; j < n; ++j)
-       if (values[min] > values[j]) min = j;
-     std::swap(values[min], values[i]);
-   }
- }
+
+
+
+        //std::cout << "nmax: " << nmax << "\n";
+        /*if (n + len > nmax) {
+           auto newnmax = nmax;
+           E * newvalues = nullptr;
+           while (n + len > newnmax) newnmax = (newnmax*12)/10 + 2;
+           newvalues = new E[newnmax];
+           for (size_t i = 0; i < n; ++i)
+            newvalues[i] = values[i];
+           delete[] values;
+           values = newvalues;
+           nmax = newnmax;
+           }
+           for (size_t i = 0; i < len; ++i)
+           if (!member(e[i]))
+            values[n++] = e[i];*/
+}
+
+template <typename E, size_t N>
+void ContDynArray<E,N>::remove(const E e[], size_t len) {
+        for (size_t i = 0; i < len; ++i) {
+                for (size_t j = 0; j < n; ++j) {
+                        if (values[j] == e[i]) {
+                                values[j] = values[--n];
+                                break;
+                        }
+                }
+        }
+}
+
+template <typename E, size_t N>
+bool ContDynArray<E,N>::member(const E &e) const {
+        if(H1[hash1(e)] == e || H2[hash2(e)] == e)
+                return true;
+        else
+                return false;
+}
+
+template <typename E, size_t N>
+E ContDynArray<E,N>::min() const {
+        if (this->empty()) throw ContDynArrayEmptyException();
+        E rc = values[0];
+        for (size_t i = 1; i < n; ++i) {
+                if (rc > values[i]) rc = values[i];
+        }
+        return rc;
+}
+
+template <typename E, size_t N>
+E ContDynArray<E,N>::max() const {
+        if (this->empty()) throw ContDynArrayEmptyException();
+        E rc = values[0];
+        for (size_t i = 1; i < n; ++i) {
+                if (values[i] > rc) rc = values[i];
+        }
+        return rc;
+}
+
+template <typename E, size_t N>
+std::ostream& ContDynArray<E,N>::print(std::ostream& o) const {
+        o << "ContDynArray [ n=" << n << " nmax=" << nmax << " values=";
+        for (size_t i = 0; i < n; ++i) o << ' ' << values[i];
+        o << " ]";
+        return o;
+}
+
+template <typename E, size_t N>
+size_t ContDynArray<E,N>::apply(std::function<void(const E &)> f, Order order) const {
+        size_t rc = 0;
+        if (order != dontcare) sort();
+        try {
+                if (order == descending) {
+                        for (size_t i = n; i--; ) {
+                                f(values[i]);
+                                ++rc;
+                        }
+                } else {
+                        for (size_t i = 0; i < n; ++i) {
+                                f(values[i]);
+                                ++rc;
+                        }
+                }
+        } catch (...) {
+        }
+        return rc;
+}
+
+template <typename E, size_t N>
+void ContDynArray<E,N>::sort() const {  // Achtung, O(n*n)
+        for (size_t i = 0; i < n; ++i) {
+                size_t min = i;
+                for (size_t j = i+1; j < n; ++j)
+                        if (values[min] > values[j]) min = j;
+                std::swap(values[min], values[i]);
+        }
+}
 
  #endif //CONTDYNARRAY_H

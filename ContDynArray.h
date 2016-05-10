75f46383e63f92f1aa390d8cfc16f81f84bf8616
Sebastian Dumbs <thesofty@gmail.com>
2016-05-10 17:42:17 +0200

    finally workgin

--- ContDynArray.h
+++ ContDynArray.h
@@ -1,5 +1,5 @@
- #ifndef CONTDYNARRAY_H
- #define CONTDYNARRAY_H
+#ifndef CONTDYNARRAY_H
+#define CONTDYNARRAY_H
 
 // ContDynArray.h
 //
@@ -10,166 +10,163 @@
 // Achtung: Da hier keine effiziente Suchdatenstruktur zum Einsatz kommt,
 // ist das Laufzeitverhalten extrem schlecht.
 
- #include <iostream>
- #include "Container.h"
- #include <math.h>       /* pow */
- #include <stdlib.h> // For random(), RAND_MAX
- #include <limits.h> //For CHAR_BIT
- #include <random> //for random number gen
- #include <algorithm> //find in vector
+#include <iostream>
+#include "Container.h"
+#include <limits.h> //For CHAR_BIT
+#include <random> //for random number gen
 
-
-
-
-class ContDynArrayEmptyException: public ContainerException {
+class ContDynArrayEmptyException : public ContainerException {
 public:
-        virtual const char * what() const noexcept override {
-                return "ContDynArray: empty";
-        }
+virtual const char * what() const noexcept override {
+        return "ContDynArray: empty";
+}
 };
 
-template <typename E, size_t N=7>
-                                class ContDynArray: public Container<E> {
-        size_t nmax;
-        size_t oldnmax = nmax;//das brauchen wir um über die alten H1 und H2 zu iterieren
-        size_t n;
+template <typename E, size_t N=16>
+class ContDynArray : public Container<E> {
+size_t nmax;
+size_t oldnmax;       //das brauchen wir um über die alten H1 und H2 zu iterieren
+size_t n;        //rename n and m
 
-        const int tMax = 10000; // max number of random walk trials
-        int t=0; //number of current walks
-        size_t q;//exponent der aktuellen tabellengröße als 2er potenz
-        E * H1;
-        E * H2;
+size_t q;       //exponent der aktuellen tabellengröße als 2er potenz
+E * H1;
+E * H2;
 
-        enum class Status: char { frei, belegt, wiederfrei };
-        Status * s1;
-        Status * s2;
+enum class Status : char { frei, belegt, wiederfrei };
+Status * s1;
+Status * s2;
 
 
-        std::random_device rd; // only used once to initialise (seed) engine
+//std::random_device rd; // only used once to initialise (seed) engine
+std::default_random_engine rd;
 
+size_t a1 = random_nmbr();        //random numbers should be huge about 18-20 digits
+size_t a2 = random_nmbr();
 
-        size_t a1 = random_nmbr(); //random numbers should be huge about 18-20 digits
-        size_t a2 = random_nmbr();
+size_t random_nmbr(){
+        //std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
+        std::uniform_int_distribution<size_t> distribution(0,SIZE_MAX-1);        // guaranteed unbiased
 
-        size_t random_nmbr(){
-                std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
-                std::uniform_int_distribution<size_t> uni(0,SIZE_MAX); // guaranteed unbiased
+        return distribution(rd)|1;
+}
 
-                return uni(rng);
+// function for calculation of hash
+size_t hash1(const E& e) const {
+        return q ? ((a1*hashValue(e))>>((CHAR_BIT*sizeof(size_t))-q)) : 0;
+}
+size_t hash2(const E& e) const {
+        return q ? ((a2*hashValue(e))>>((CHAR_BIT*sizeof(size_t))-q)) : 0;
+}
+
+//rewrite with pot
+size_t pot(size_t size) {
+        size_t h=0;
+        size_t th=h;
+        while(size>(h)) {
+                ++th;
+                h=(1<<th);
         }
+        return (h);
+}
 
-        // function for calculation of hash
-        size_t hash1(const E& e) const {
-                return q?((a1*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q)):0;
+size_t expt(size_t size) {
+        size_t h=0;
+        size_t th=h;
+        while(size>(h)) {
+                ++th;
+                h=(1<<th);
         }
-        size_t hash2(const E& e) const {
-                return q?((a2*hashValue(e))>>(CHAR_BIT*sizeof(size_t)-q)):0;
-        }
+        return (th);
+}
 
-        size_t pot(size_t size) {
-                size_t h=0;
-                size_t th=h;
-                while(size>(h)) {
-                        ++th;
-                        h=(1<<th);
-                }
-                return (h);
-        }
+void add_(const E&, size_t t);
 
-        size_t expt(size_t size) {
-                size_t h=0;
-                size_t th=h;
-                while(size>(h)) {
-                        ++th;
-                        h=(1<<th);
-                }
-                return (th);
-        }
-
-        void add_(const E&);
-
-        void resize(size_t nmaxnew);
-        void rehash();
+void resize(size_t nmaxnew);
+void rehash(const E&);
 
 
-        bool member1_(const E& e) const;
-        bool member2_(const E& e) const;
+bool member1_(const E& e) const;
+bool member2_(const E& e) const;
 
-        void sort() const;
+void sort() const;
 public:
-        ContDynArray() : nmax {(N<1) ? 2 : pot(N)}, n {0},q {expt(N)}, H1 {new E[this->nmax]()}, H2 {new E[this->nmax]()},s1 {new Status[this->nmax]()},s2 {new Status[this->nmax]()} {
-        }
-        ContDynArray(std::initializer_list<E> el) : ContDynArray() {
-                for (auto e: el) add(e);
-        }
+ContDynArray() : nmax {(N<1) ? 2 : pot(N)}, n {0},q {expt(N)}, H1 {new E[this->nmax]()}, H2 {new E[this->nmax]()},s1 {new Status[this->nmax]()},s2 {new Status[this->nmax]()} {
+}
+ContDynArray(std::initializer_list<E> el) : ContDynArray() {
+        for (auto e: el) add(e);
+}
 
-        virtual ~ContDynArray() {
-                delete[] H1;
-                delete[] H2;
-                delete[] s1;
-                delete[] s2;
-        }
+virtual ~ContDynArray() {
+        delete[] H1;
+        delete[] H2;
+        delete[] s1;
+        delete[] s2;
+}
 
-        using Container<E>::add;
-        virtual void add(const E e[], size_t len) override;
+using Container<E>::add;
+virtual void add(const E e[], size_t len) override;
 
-        using Container<E>::remove;
-        virtual void remove(const E e[], size_t len) override;
+using Container<E>::remove;
+virtual void remove(const E e[], size_t len) override;
 
-        virtual bool member(const E& e) const override;
-        virtual size_t size() const override {
-                return n;
-        }
+virtual bool member(const E& e) const override;
+virtual size_t size() const override {
+        return n;
+}
 
-        virtual E min() const override;
-        virtual E max() const override;
+virtual E min() const override;
+virtual E max() const override;
 
-        virtual std::ostream& print(std::ostream& o) const override;
+virtual std::ostream& print(std::ostream& o) const override;
 
-        virtual size_t apply(std::function<void(const E &)> f, Order order = dontcare) const override;
+virtual size_t apply(std::function<void(const E &)> f, Order order = dontcare) const override;
 };
 
 template <typename E, size_t N>
-void ContDynArray<E,N>::add_(const E &e) {
-        size_t pos1 = hash1(e);
+void ContDynArray<E,N>::add_(const E &e,size_t t) {//Try with a loop now as recurions seem to increase stack size way beyond limit
+  if (n  > (nmax*0.48))
+          resize(1<<(++q));
 
-        if (s1[pos1] != Status::belegt) {//wenn h1 and entsprechender stelle frei ist
-                H1[pos1] = e; //insert elemnt into  H1
-                s1[pos1] = Status::belegt;
-        }else{//wenn h1 and entsprechender stelle belegt ist
-                E tmp = H1[pos1]; //store the element from H1 in a tmp variable
-                H1[pos1] = e; //insert elemnt into  H1
-                if(s2[hash2(tmp)] != Status::belegt) {//h2 and entsprechender stelle ist frei
-                        H2[hash2(tmp)] = tmp; //move the conflicting element from tmp to H2
-                        s2[hash2(tmp)] = Status::belegt;
+        E element = e;
+        while(t) {//as long as we do not have to rehash
+                size_t pos1 = hash1(element);
+
+                if (s1[pos1] != Status::belegt) {//wenn h1 and entsprechender stelle frei ist
+                        H1[pos1] = element; //insert elemnt into  H1
+                        s1[pos1] = Status::belegt;
+                        ++n;
+                        break;
                 }
-                else{ //both H1 and H2 have something at that their corresponding positions we now need to juggle this arround
-                        E tmp2 = H2[hash2(tmp)]; //store the element from H2 in a tmp variable
-                        H2[hash2(tmp)] = tmp; //move the conflicting element from tmp to H2
-
-                        if(t > tMax) {//do we need to rehash?
-                          //std::cout << "We need to rehash!\n" << "t: " << t << "\n";
-                          t = 0;//reset t since we have rehashed
-                          rehash();
+                else{//wenn h1 and entsprechender stelle belegt ist
+                        E tmp = H1[pos1]; //store the element from H1 in a tmp variable
+                        H1[pos1] = element; //insert elemnt into  H1
+                        if(s2[hash2(tmp)] != Status::belegt) {//h2 and entsprechender stelle ist frei
+                                H2[hash2(tmp)] = tmp; //move the conflicting element from tmp to H2
+                                s2[hash2(tmp)] = Status::belegt;
+                                ++n;
+                                break;
                         }
-                        t++;
-                        add_(tmp2);//call add again to store the element from the tmp2 variable
+                        else{ //both H1 and H2 have something at that their corresponding positions we now need to juggle this arround
+                                element = H2[hash2(tmp)]; //store the element from H2 in a tmp variable
+                                H2[hash2(tmp)] = tmp; //move the conflicting element from tmp to H2
+                                --t;
+                        }
                 }
+        }
+        if(t==0) {
+                //std::cout << "We need to rehash! \n";
+                rehash(element);
         }
 }
 
 template <typename E, size_t N>
 void ContDynArray<E,N>::add(const E e[], size_t len) {
-        if (n + len > nmax*0.1){
-            resize(size_t(pow(2,++q)));
-        }
+
 
 
         for (size_t i = 0; i < len; ++i) { //go through all values we where given
                 if (!member(e[i])) { //only execute ifhe element is neither in H1 nor in H2
-                        t = 0;//set current walks to 0
-                        add_(e[i]);
-                        ++n;
+                        add_(e[i],(nmax/2+1)); //count=tabellesize/2+1;
                 }
         }
 }
@@ -188,13 +185,14 @@ void ContDynArray<E,N>::resize(size_t nmaxnew) {
         H2 = new E[nmax]();
         s1 = new Status[nmax]();
         s2 = new Status[nmax]();
+        n=0;
         //end of save old stuff and allocate space for new stuff
 
         //write old elements into new hashtables
         for (size_t i = 0; i < oldnmax; ++i)
-                if (old_s1[i] == Status::belegt) add_(old_H1[i]);
+                if (old_s1[i] == Status::belegt) add(old_H1[i]);
         for (size_t i = 0; i < oldnmax; ++i)
-                if (old_s2[i] == Status::belegt) add_(old_H2[i]);
+                if (old_s2[i] == Status::belegt) add(old_H2[i]);
 
         //delete temp arrays
         delete[] old_H1;
@@ -204,10 +202,11 @@ void ContDynArray<E,N>::resize(size_t nmaxnew) {
 }
 
 template <typename E, size_t N>
-void ContDynArray<E,N>::rehash() {
+void ContDynArray<E,N>::rehash(const E &e) {
         a1 = random_nmbr(); //random numbers should be huge about 18-20 digits
         a2 = random_nmbr();
         resize(nmax);//call resize to rewrite all elements
+        add(e);
 
 }
 
@@ -216,8 +215,11 @@ void ContDynArray<E,N>::remove(const E e[], size_t len) {
         for (size_t i = 0; i < len; ++i) {
                 if (member1_(e[i])) {
                         s1[hash1(e[i])] = Status::wiederfrei;
-                }else if (member2_(e[i])) {
+                        --n;
+                }
+                else if (member2_(e[i])) {
                         s2[hash2(e[i])] = Status::wiederfrei;
+                        --n;
                 }
         }
 }
@@ -230,7 +232,6 @@ bool ContDynArray<E,N>::member1_(const E &e) const {
         else{
                 return false;
         }
-
 }
 
 template <typename E, size_t N>
@@ -240,8 +241,8 @@ bool ContDynArray<E,N>::member2_(const E &e) const {
         }
         else{
                 return false;
-        }
 
+        }
 }
 
 template <typename E, size_t N>
@@ -252,7 +253,6 @@ bool ContDynArray<E,N>::member(const E &e) const {
         else{
                 return false;
         }
-
 }
 
 template <typename E, size_t N>
@@ -302,32 +302,12 @@ std::ostream& ContDynArray<E,N>::print(std::ostream& o) const {
 template <typename E, size_t N>
 size_t ContDynArray<E,N>::apply(std::function<void(const E &)> f, Order order) const {
         size_t rc = 0;
-        /*if (order != dontcare) sort();
-           try {
-                if (order == descending) {
-                        for (size_t i = n; i--; ) {
-                                f(values[i]);
-           ++rc;
-                        }
-                } else {
-                        for (size_t i = 0; i < n; ++i) {
-                                f(values[i]);
-           ++rc;
-                        }
-                }
-           } catch (...) {
-           }*/
+
         return rc;
 }
 
 template <typename E, size_t N>
 void ContDynArray<E,N>::sort() const {  // Achtung, O(n*n)
-        /*for (size_t i = 0; i < n; ++i) {
-                size_t min = i;
-                for (size_t j = i+1; j < n; ++j)
-                        if (values[min] > values[j]) min = j;
-                std::swap(values[min], values[i]);
-           }*/
-}
 
- #endif //CONTDYNARRAY_H
+}
+#endif //CONTDYNARRAY_H
